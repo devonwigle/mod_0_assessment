142dda6e806e61a90056fb55b96c0d6710190784
Devon Wigle <devonwigle@gmail.com>
2021-09-03 10:01:14 -0500

    Add developer object

---
+++ models/modelB.js
@@ -0,0 +1,10 @@
+var developer = {
+  name: "Sam",
+  favoriteLanguage: "python",
+  isFrontEnd: false,
+  usesAppleProducts: true,
+  yearsDeveloping: 3.5,
+  numberOfGitRepos: 122,
+  companiesWorkedFor: ['Apple', 'Facebook', 'Starbucks'],
+  knownLanguages: ['C#', 'python', 'php']
+};
