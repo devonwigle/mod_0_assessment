621c3872d36718588d706ddc788c28980fc41f2e
Devon Wigle <devonwigle@gmail.com>
2021-09-03 09:30:00 -0500

    Complete program specific challenge

---
+++ variables.js
@@ -0,0 +1,9 @@
+var movie = "101 Dalmatians";
+var isEntertaining = true;
+movie = "Cruella";
+var num4 = 13;
+var num6 = 35;
+var product = num6 * num4;
+var favoriteFoods = ['grapes', 'craisins', 'pirouettes'];
+favoriteFoods.shift();
+console.log(movie.length);
