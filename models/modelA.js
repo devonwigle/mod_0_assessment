5b5dbf444e74c87fc56ec1acbb4aad980543d1f0
Devon Wigle <devonwigle@gmail.com>
2021-09-03 09:51:56 -0500

    Add dinosaur object

---
+++ models/modelA.js
@@ -0,0 +1,10 @@
+var dinosaur = {
+  totalLengthInInches: 20,
+  weightInLbs: 2.2,
+  scientificName: "Archaeopteryx",
+  eraLived: "Tithonian stage of late Jurassic period" ,
+  hasFeathers: true,
+  eatsMeat: true,
+  wingDescription: ["asymmetric shape", "feathered", "broad", "rounded ends"],
+  footDescription: ['extendable claw on 2nd toe', 'first toe at side of foot', 4],
+}
